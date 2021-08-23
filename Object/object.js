025b657322e975572614c2dff65da099b180e4d7
Mrudula-Kulkarni <58851969+Mrudula-Kulkarni@users.noreply.github.com>
2021-08-23 19:37:47 +0530

    Object Literal and this

--- Object/object.js
+++ Object/object.js
@@ -28,3 +28,21 @@ var facebook = {
 
 console.log(facebook.ceo.firstName);
 
+// Object literals and "this"
+var literalCircle = {
+  radius: 10,
+
+  getArea: function () {
+    var self = this;
+    console.log(this);
+
+    var increaseRadius = function () {
+      self.radius = 20;
+    };
+    increaseRadius();
+    console.log(this.radius);
+
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+};
+console.log(literalCircle.getArea());
