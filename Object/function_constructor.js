5a122959fe34b76bec8e5aab25baa0ac1fd66c38
Mrudula-Kulkarni <58851969+Mrudula-Kulkarni@users.noreply.github.com>
2021-08-23 19:13:29 +0530

    Function Constructor and Prototype

---
+++ Object/function_constructor.js
@@ -0,0 +1,18 @@
+// Function constructors
+function Circle (radius) {
+    this.radius = radius;
+  }
+  
+  Circle.prototype.getArea = 
+    function () {
+      return Math.PI * Math.pow(this.radius, 2);
+    };
+  
+  
+  var myCircle = new Circle(10);
+  console.log(myCircle.getArea());
+  
+  var myOtherCircle = new Circle(20);
+  console.log(myOtherCircle);
+
+  // Prototype cannot be placed in function Constructor block as that would lead to waste of processing
